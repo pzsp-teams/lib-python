a8cb62afb9a4b727e8ac161aef001aa1a9f87b09
Michal <01187288@pw.edu.pl>
2025-12-23 12:03:14 +0100

    feat: implemented most of teams service in python

---
+++ python/teams_lib_pzsp2_z1/services/teams.py
@@ -0,0 +1,117 @@
+from teams_lib_pzsp2_z1.model.team import Team
+from teams_lib_pzsp2_z1.services.base_service import BaseService
+
+
+class TeamsService(BaseService):
+    def get(self, teamRef: str) -> Team:
+        response = self.client.execute(
+            cmd_type="request",
+            method="getTeam",
+            params={
+                "teamRef": teamRef,
+            },
+        )
+        return Team(
+            ID=response["ID"],
+            DisplayName=response["DisplayName"],
+            Description=response["Description"],
+            IsArchived=(True if response["IsArchived"] else False),
+            Visibility=response["Visibility"],
+        )
+
+    def list_my_joined(self) -> list[Team]:
+        response = self.client.execute(
+            cmd_type="request",
+            method="listMyJoined",
+            params={},
+        )
+        return [
+            Team(
+                ID=team["ID"],
+                DisplayName=team["DisplayName"],
+                Description=team["Description"],
+                IsArchived=(True if team["IsArchived"] else False),
+                Visibility=team["Visibility"],
+            )
+            for team in response
+        ]
+
+    # def update //TODO
+
+    def create_via_group(
+        self, display_name: str, mail_nickname: str, visibility: str
+    ) -> Team:
+        response = self.client.execute(
+            cmd_type="request",
+            method="createTeamViaGroup",
+            params={
+                "displayName": display_name,
+                "mailNickname": mail_nickname,
+                "visibility": visibility,
+            },
+        )
+        return Team(
+            ID=response["ID"],
+            DisplayName=response["DisplayName"],
+            Description=response["Description"],
+            IsArchived=(True if response["IsArchived"] else False),
+            Visibility=response["Visibility"],
+        )
+
+    def create_from_template(
+        self,
+        display_name: str,
+        description: str,
+        owners: list[str],
+    ) -> str:
+        response = self.client.execute(
+            cmd_type="request",
+            method="createTeamFromTemplate",
+            params={
+                "displayName": display_name,
+                "description": description,
+                "owners": owners,
+            },
+        )
+        return response["id"]
+
+    def archive(self, teamRef: str, spo_read_only_from_members: bool) -> bool:
+        response = self.client.execute(
+            cmd_type="request",
+            method="archiveTeam",
+            params={
+                "teamRef": teamRef,
+                "spoReadOnlyFromMembers": spo_read_only_from_members,
+            },
+        )
+        return response == "archived"
+
+    def unarchive(self, teamRef: str) -> bool:
+        response = self.client.execute(
+            cmd_type="request",
+            method="unarchiveTeam",
+            params={
+                "teamRef": teamRef,
+            },
+        )
+        return response == "unarchived"
+
+    def delete(self, teamRef: str) -> bool:
+        response = self.client.execute(
+            cmd_type="request",
+            method="deleteTeam",
+            params={
+                "teamRef": teamRef,
+            },
+        )
+        return response == "deleted"
+
+    def restore_deleted(self, teamRef: str) -> str:
+        response = self.client.execute(
+            cmd_type="request",
+            method="restoreDeletedTeam",
+            params={
+                "teamRef": teamRef,
+            },
+        )
+        return response["id"]
