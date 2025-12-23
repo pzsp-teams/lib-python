f9fd045fd3b938464fe6e46c93edea21266f645f
Michal <01187288@pw.edu.pl>
2025-12-23 18:27:14 +0100

    feat: added test to createTeamViaGroup (torture)

--- python/tests/fake_server/data.py
+++ python/tests/fake_server/data.py
@@ -1,11 +1,18 @@
 from dataclasses import dataclass
 from teams_lib_pzsp2_z1.model.team import Team
 from teams_lib_pzsp2_z1.model.channel import Channel
+from dataclasses import field
 
 @dataclass
 class FakeServerData:
     teams: list[Team]
     channels: dict[str, list[Channel]]
+
+    newGroupID: str
+    newTeamName: str
+    newGroupMailNickname: str
+    newTeamVisibility: str
+    potentialTeams: list[Team] = field(default_factory=list)
 
     def __init__(self) -> FakeServerData:
         self.teams = [
@@ -38,6 +45,12 @@ class FakeServerData:
                 ),
             ],
         }
+        self.newGroupID = "group-789-ghi"
+        self.newTeamName = "New Team"
+        self.newGroupMailNickname = "new-team-nickname"
+        self.newTeamVisibility = "private"
+        self.potentialTeams = []
+
 
     def get_myJoinedTeams_response(self) -> dict:
         return {
@@ -73,6 +86,49 @@ class FakeServerData:
             "isArchived": team.IsArchived,
             "visibility": team.Visibility,
         } if (team := next((t for t in self.teams if t.ID == team_id), None)) else None
+
+    def get_createGroup_response(self, request_json: dict) -> dict:
+        visibility = request_json.get("visibility").lower()
+        if visibility not in ("private", "public"):
+            visibility = "private"
+
+        self.potentialTeams.append(
+            Team(
+                ID=self.newGroupID,
+                DisplayName=request_json.get("displayName"),
+                Description=request_json.get("description"),
+                IsArchived=False,
+                Visibility=visibility,
+            )
+        )
+
+        return {
+            "id": self.newGroupID,
+            "displayName": request_json.get("displayName"),
+            "description": request_json.get("displayName"),
+            "visibility": visibility,
+            "groupTypes": ["Unified"],
+            "mailEnabled": True,
+            "mailNickname": request_json.get("mailNickname"),
+            "securityEnabled": False,
+            "createdDateTime": "2024-01-01T00:00:00Z",
+        }
+
+    def get_createTeamViaGroup_response(self, group_id: str) -> dict:
+        team = next((t for t in self.potentialTeams if t.ID == group_id), None)
+        if not team:
+            return {}
+
+        self.teams.append(team)
+
+        return {
+            "id": team.ID,
+            "displayName": team.DisplayName,
+            "description": team.Description,
+            "isArchived": team.IsArchived,
+            "visibility": team.Visibility,
+        }
+
 
 
 
