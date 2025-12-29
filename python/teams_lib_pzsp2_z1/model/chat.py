de296a62b493f935bda018dcf3dce6caf986cd6f
Michal <01187288@pw.edu.pl>
2025-12-29 12:06:49 +0100

    feat: prepared structure for chat service in python

---
+++ python/teams_lib_pzsp2_z1/model/chat.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class ChatType(Enum):
+    ONEONONE = "one-on-one"
+    GROUP = "group"
+
+
+@dataclass
+class Chat:
+    ID: str
+    Type: ChatType
+    IsHidden: bool
+    Topic: str
---
+++ python/teams_lib_pzsp2_z1/services/chats.py
@@ -0,0 +1,12 @@
+from teams_lib_pzsp2_z1.model.chat import Chat, ChatType
+from teams_lib_pzsp2_z1.model.member import Member
+from teams_lib_pzsp2_z1.model.message import (
+    Message,
+    MessageBody,
+    MessageContentType,
+    MessageFrom,
+)
+from teams_lib_pzsp2_z1.services.base_service import BaseService
+
+
+class ChatsService(BaseService):
